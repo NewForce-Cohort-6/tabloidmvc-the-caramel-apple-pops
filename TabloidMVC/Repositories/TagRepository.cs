c1f861601532024e970cbb2094e249b8406b176a
Jeannette Geib <jeannettegeib@gmail.com>
2022-10-31 11:55:18 -0400

    Woohoo!

--- TabloidMVC/Repositories/TagRepository.cs
+++ TabloidMVC/Repositories/TagRepository.cs
@@ -5,8 +5,33 @@ using TabloidMVC.Models;
 namespace TabloidMVC.Repositories
 {
     public class TagRepository: BaseRepository, ITagRepository
-
     {
+        public TagRepository(IConfiguration config): base(config) { }
 
+        public List<Tag> GetAll()
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "SELECT Id, Name FROM Tag ORDER BY Name";
+                    var reader=cmd.ExecuteReader();
+
+                    List<Tag> tags = new List<Tag>();
+
+                    while (reader.Read())
+                    {
+                        tags.Add(new Tag()
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            Name = reader.GetString(reader.GetOrdinal("Name"))
+                        });
+                    }
+                    reader.Close();
+                    return tags;
+                }
+            }
+        }
     }
 }
